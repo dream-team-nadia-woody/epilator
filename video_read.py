cffad6d8f53d5ebc2ba6693186f47f60a86b5192
Nadia Paz <nadina.abouhamdan@gmail.com>
2023-03-06 13:03:08 -0600

    add links

--- video_read.py
+++ video_read.py
@@ -1,12 +1,26 @@
 from typing import Union
 from pytube import YouTube as yt
 
-def download_video(url:str,path:str = 'videos/', filename:Union[str,None] = None)->bool:
-    video = yt(url)
-    stream = video.streams.get_highest_resolution()
-    try:
-         stream.download(path)
-    except Exception:
-         return False
-    return True
+links = [
+     'https://www.youtube.com/watch?v=4LQLvgXLguk',
+     'https://www.youtube.com/watch?v=CIHun1gx7zU',
+     'https://www.youtube.com/watch?v=6dSkaRnnc5s',
+     'https://www.youtube.com/watch?v=71-tMMtVWMI&t=17s',
+     'https://www.youtube.com/watch?v=sWu7Sok3kok',
+     'https://www.youtube.com/watch?v=6UZJfjBcUKM'
+]
 
+def download_video(url:str,path:str = 'videos/', filename:Union[str,None] = None)->bool:
+     '''
+
+     '''
+     video = yt(url)
+     stream = video.streams.get_highest_resolution()
+     try:
+          stream.download(path)
+     except Exception:
+          return False
+     return True
+
+for link in links:
+     download_video(link)
