f400b9dae38360b93413768e19b616b8642fa3b4
Woody Sims <sims.woodrow@gmail.com>
2023-03-06 16:54:37 -0600

    videos can read in

---
+++ video.py
@@ -0,0 +1,53 @@
+from typing import Tuple, Union
+import cv2 as cv
+import numpy as np
+import pandas as pd
+from numpy.typing import NDArray
+from dataclasses import dataclass
+from collections import deque
+
+
+class Video:
+    video: cv.VideoCapture
+
+    def __init__(self, path: str):
+        self.video = cv.VideoCapture(path)
+
+    def __del__(self):
+        self.video.release()
+
+    def __iter__(self):
+        if not self.video.isOpened():
+            raise StopIteration
+        return self
+
+    def __next__(self):
+        end, frame = self.video.read()
+        if not end:
+            raise StopIteration
+        frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+        return frame
+
+
+def get_video(path: str) -> Tuple[NDArray, int]:
+    vid = Video(path)
+    fps = vid.video.get(cv.CAP_PROP_FPS)
+    video = np.fromiter(vid, np.ndarray)
+    return np.stack(video), int(round(fps))
+
+
+def get_vid_df(path: str) -> pd.DataFrame:
+    vid,fps = get_video(path)
+    frames = vid.shape[0]
+    height = vid.shape[1]
+    width = vid.shape[2]
+    df = pd.DataFrame(vid.reshape((-1,3)))
+    df['frame'] = df.index // (width * height)
+    df['x'] = df.index % width
+    df['y'] = df.index // height
+    df = df.set_index(['frame','y','x']).rename(columns={
+        0:'hue',
+        1:'saturation',
+        2:'value',
+    })
+    return df,fps
--- video_read.py
+++ video_read.py
@@ -5,7 +5,7 @@ links = [
      'https://www.youtube.com/watch?v=4LQLvgXLguk',
      'https://www.youtube.com/watch?v=CIHun1gx7zU',
      'https://www.youtube.com/watch?v=6dSkaRnnc5s',
-     'https://www.youtube.com/watch?v=71-tMMtVWMI&t=17s',
+     'https://www.youtube.com/watch?v=71-tMMtVWMI',
      'https://www.youtube.com/watch?v=sWu7Sok3kok',
      'https://www.youtube.com/watch?v=6UZJfjBcUKM'
 ]
@@ -30,7 +30,7 @@ def download_video(url:str,path:str = 'videos/', filename:Union[str,None] = None
      return True
 
 for link in links:
-     download_video(link)
+     download_video(link,'videos/constant/')
 
 for link in hidden_hazard:
-     download_video(link)
+     download_video(link,'videos/hidden_hazards/')
