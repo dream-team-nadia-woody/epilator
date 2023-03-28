c988cc09f5a025eaf4f3c4cb8ef304c78c00dbad
Woody Sims <sims.woodrow@gmail.com>
2023-03-28 15:29:39 -0500

    Revert "Revert "Merge remote-tracking branch 'origin/main' into woody-branch""
    This reverts commit e89a5353bfff5ac0005e1b5b0e9c371abcfdf534.

--- video/conversion.py
+++ video/conversion.py
@@ -2,16 +2,17 @@ import cv2 as cv
 from enum import Enum
 from dataclasses import dataclass
 from typing import Union
+import numpy as np
 
 
 @dataclass
 class Converter:
-    load: Union[int, None]
-    display: Union[int, None]
+    load: int
+    display: int
 
 
 class Conversions(Enum):
     HLS = Converter(cv.COLOR_BGR2HLS, cv.COLOR_HLS2RGB)
     HSV = Converter(cv.COLOR_BGR2HSV, cv.COLOR_HSV2RGB)
-    RGB = Converter(cv.COLOR_BGR2RGB, None)
-    BGR = Converter(None, cv.COLOR_BGR2RGB)
+    RGB = Converter(cv.COLOR_BGR2RGB, 0)
+    BGR = Converter(0, cv.COLOR_BGR2RGB)
---
+++ video/frame.py
@@ -0,0 +1,39 @@
+import cv2 as cv
+import numpy as np
+from numpy.typing import ArrayLike
+from PIL import Image
+
+from video.conversion import Converter
+from video.videolike import VideoLike
+
+
+class Frame(VideoLike):
+    '''
+    A wrapper class representing the single frame of
+    a video. This prevents issues with reshaping
+    of the array.
+    '''
+    conversion: Converter
+    fps: float
+    frame_no: np.uint64
+    seconds: np.float128
+
+    def __init__(self, frame: ArrayLike, fps: float, conversion: Converter,
+                 frame_no: np.uint64) -> None:
+        super().__init__(frame, None, conversion)
+        self.frame_no = frame_no
+        self.seconds = np.float128(frame / fps)
+        self.fps = fps
+
+    def show(self) -> Image:
+        '''
+        Returns the frame as a PIL Image object
+        ## Parameters:
+        None
+        ## Returns:
+        a PIL Image object
+        '''
+        converted_image = self._vid
+        if self.converter.display > 0:
+            cv.cvtColor(self._vid, self.converter.display)
+        return Image.fromarray(converted_image)
